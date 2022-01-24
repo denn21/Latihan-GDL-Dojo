a2e40cc131bac5541eb67dc023281d990715e78a
denn21 <87965245+denn21@users.noreply.github.com>
2022-01-24 17:51:15 +0700

    Test Tugas GDL Dojo

---
+++ Tugas GDL Dojo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TugasGDL : MonoBehaviour
+{
+    string[] namaAnggota = {"Jamal", "Adib", "Bane", "Freya"}
+    
+    Debug.Log($"Anggota terbaik adalah " + namaAnggota[1]);
+    
+    for (int i=0; i < 5; i++)
+    {
+    
+    }
+}
